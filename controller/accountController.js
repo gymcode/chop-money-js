9106206a1fe8730d89680ce77a423ed501a29a84
gymcode <kyleabs20@gmail.com>
2022-11-28 16:34:37 +0000

    fixed error on the msisdn validation

--- controller/accountController.js
+++ controller/accountController.js
@@ -1,6 +1,6 @@
 const Account = require("../models/Account")
+const { CountryMsisdnValidation } = require("../utils/msisdnValidation")
 const Transaction = require("../models/Transaction")
-const User = require("../models/User")
 const { wrapFailureResponse, wrapSuccessResponse } = require("../shared/response")
 const {
     diff_Days_Weeks, 
@@ -14,97 +14,102 @@ creating an account
 */
 exports.createAccount = async(req, res)=>{
 
-    const request = req.body
-    const {user, token} = res.locals.user_info   
-
-    if (user == null)
-        return wrapFailureResponse(res, 404, "User not found", null)
-
-    let beneficiaryContact = ""
-    // check the status of is beneficiary 
-    if (request.isBeneficiary) {
-        const { error, msg } = CountryMsisdnValidation(request.beneficiaryContact, request.countryCode)
-        if (error) return wrapFailureResponse(res, 422, msg, null)
-        beneficiaryContact = msg
-    }
-    const numberOfDays = 31
-    const totalHours = numberOfDays * 24 
-    let endDate = request.endDate == "" ? getCurrentDateTime(totalHours) : request.endDate
-
-    // create account for user
-    const accountInput = new Account({
-        chopMoneyOwner: request.chopMoneyOwner,
-        isBeneficiary: request.isBeneficiary,
-        beneficiaryContact: beneficiaryContact,
-        beneficiaryName: request.beneficiaryName,
-        ownerContact: user.msisdn,
-        ownerName: user.username,
-        payFrequency: request.payFrequency,
-        payFrequencyAmount: request.payFrequencyAmount,
-        startDate: request.startDate,
-        endDate: endDate,
-        payTime: request.payTime,
-        totalPayAmount: request.totalPayAmount,
-    })
-
-    const accountResponse = await accountInput.save()
+    try{
+        const request = req.body
+        const {user, token} = res.locals.user_info   
     
-    if (accountResponse == null) return wrapFailureResponse(res, 500, "Could not insert", null)
-
-    // update the user data with the account details
-    user.accounts.push(accountResponse._id)
-    user.save()
-
-    // get the difference between the dates  
-    const days = diff_Days_Weeks(request.startDate, endDate)
-    const weeks = diff_Days_Weeks(request.startDate, endDate, 7)
-    const biWeeks = diff_Days_Weeks(request.startDate, endDate, 14)
-
-    let objectArr = [];
-    switch (request.payFrequency) {
-        case 'DAILY'.toUpperCase():
-            objectArr = transactionObject(
-                request.customizedArray,
-                request.payTime, 
-                days, 
-                request.payFrequencyAmount, 
-                1, accountInput._id)
-            break;
+        if (user == null)
+            return wrapFailureResponse(res, 404, "User not found", null)
     
-        case 'WEEKLY'.toUpperCase(): 
-            objectArr = transactionObject(
-                request.customizedArray,
-                request.payTime, 
-                weeks, 
-                request.payFrequencyAmount, 
-                7, accountInput._id)
-            break;
-
-        case 'BI-WEEKLY'.toUpperCase(): 
-            objectArr = transactionObject(
-                request.customizedArray,
-                request.payTime, 
-                biWeeks, 
-                request.payFrequencyAmount, 
-                14, accountInput._id)
-            break;
-
-        default:
-            console.log("it got here")
-            break;
+        let beneficiaryContact = ""
+        // check the status of is beneficiary 
+        if (request.isBeneficiary) {
+            console.log(request.beneficiaryContact)
+            const { error, msg } = CountryMsisdnValidation(request.beneficiaryContact, request.countryCode)
+            if (error) return wrapFailureResponse(res, 422, msg, null)
+            beneficiaryContact = msg
+        }
+        const numberOfDays = 31
+        const totalHours = numberOfDays * 24 
+        let endDate = request.endDate == "" ? getCurrentDateTime(totalHours) : request.endDate
+    
+        // create account for user
+        const accountInput = new Account({
+            chopMoneyOwner: request.chopMoneyOwner,
+            isBeneficiary: request.isBeneficiary,
+            beneficiaryContact: beneficiaryContact,
+            beneficiaryName: request.beneficiaryName,
+            ownerContact: user.msisdn,
+            ownerName: user.username,
+            payFrequency: request.payFrequency,
+            payFrequencyAmount: request.payFrequencyAmount,
+            startDate: request.startDate,
+            endDate: endDate,
+            payTime: request.payTime,
+            totalPayAmount: request.totalPayAmount,
+        })
+    
+        const accountResponse = await accountInput.save()
+        
+        if (accountResponse == null) return wrapFailureResponse(res, 500, "Could not insert", null)
+    
+        // update the user data with the account details
+        user.accounts.push(accountResponse._id)
+        user.save()
+    
+        // get the difference between the dates  
+        const days = diff_Days_Weeks(request.startDate, endDate)
+        const weeks = diff_Days_Weeks(request.startDate, endDate, 7)
+        const biWeeks = diff_Days_Weeks(request.startDate, endDate, 14)
+    
+        let objectArr = [];
+        switch (request.payFrequency) {
+            case 'DAILY'.toUpperCase():
+                objectArr = transactionObject(
+                    request.customizedArray,
+                    request.payTime, 
+                    days, 
+                    request.payFrequencyAmount, 
+                    1, accountInput._id)
+                break;
+        
+            case 'WEEKLY'.toUpperCase(): 
+                objectArr = transactionObject(
+                    request.customizedArray,
+                    request.payTime, 
+                    weeks, 
+                    request.payFrequencyAmount, 
+                    7, accountInput._id)
+                break;
+    
+            case 'BI-WEEKLY'.toUpperCase(): 
+                objectArr = transactionObject(
+                    request.customizedArray,
+                    request.payTime, 
+                    biWeeks, 
+                    request.payFrequencyAmount, 
+                    14, accountInput._id)
+                break;
+    
+            default:
+                console.log("it got here")
+                break;
+        }
+    
+        Transaction.insertMany(objectArr)
+            .then(function(data){
+                // data.map(({_id})=>{
+                //     accountResponse.transactions.push(_id)
+                // })
+                wrapSuccessResponse(res, 200, accountResponse, null, token)
+            })
+            .catch(function(err){
+                console.error("an error occured", err)
+            })
+    
+    }catch(error){
+        return wrapFailureResponse(res, 500, error.message, error)
     }
-
-    Transaction.insertMany(objectArr)
-        .then(function(data){
-            // data.map(({_id})=>{
-            //     accountResponse.transactions.push(_id)
-            // })
-            wrapSuccessResponse(res, 200, accountResponse, null, token)
-        })
-        .catch(function(err){
-            console.error("an error occured", err)
-        })
-
 }
 
 exports.withdrawCash = async (req, res)=>{
