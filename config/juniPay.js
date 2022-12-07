4b49819b158d15b0508dabebd8414f451eb95c33
gymcode <kyleabs20@gmail.com>
2022-12-07 23:40:44 +0000

    payment integrations

---
+++ config/juniPay.js
@@ -0,0 +1,13 @@
+const axios = require("axios")
+
+async function JuniPayPayment(msisdn, message){
+    try {
+        const authKey = process.env.NALO_SMS_AUTH_KEY
+        const smsUri = `https://sms.nalosolutions.com/smsbackend/clientapi/Resl_Nalo/send-message/?key=${authKey}&type=0&destination=${msisdn}&dlr=1&source=Chopmoney&message=${message}`
+        // smsUri = `https://sms.nalosolutions.com/smsbackend/clientapi/Resl_Nalo/send-message/?username=${username}&password=${password}&type=0&destination=233268211334&dlr=1&source=NALO&message=${message}`
+        const response = await axios.get(smsUri)
+        console.log(response.data)
+    } catch (error) {
+        console.error(error)
+    }
+}
