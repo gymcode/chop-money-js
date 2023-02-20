e87e688582eec257c2b02b0cc4c3fc03b1b78317
gymcode <kyleabs20@gmail.com>
2023-02-20 23:44:22 +0000

    starting in app notifications

---
+++ models/InAppNotification.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const inAppNotificationSchema = new Schema(
+  {
+    recipient: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+    version: { type: Number, default: 1 },
+  },
+  { timestamps: true }
+);
+
+const model = mongoose.model("inAppNotifications", inAppNotificationSchema);
+
+module.exports = model;
