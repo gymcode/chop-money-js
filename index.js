6b66e1c7df2866c19bc6b7a6c8b432528140ca00
gymcode <kyleabs20@gmail.com>
2023-12-05 19:52:18 +0000

    updated it with cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { Database_Connection } = require("./services/databaseConfig");
 const { config } = require("dotenv");
 const { BASE_URL } = require("./shared/constants");
 const cron = require("node-cron");
+const cors = require("cors")
 const {
   CronNotificatioController,
   CronStatusCheckController,
@@ -15,6 +16,7 @@ const port = process.env.PORT || "8080";
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 //routesconfig()
 const userRoutes = require("./routes/userRoute");
